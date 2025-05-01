86e5217ae4a376a4e233f7677893b8b012d0a1cc
Anooshakhalid <144916298+Anooshakhalid@users.noreply.github.com>
2025-05-01 13:40:40 +0500

    created semaphore and mutex class

---
+++ spring-workers.py
@@ -0,0 +1,48 @@
+# Simple Semaphore
+class Semaphore:
+    def __init__(self, value=0):
+        self.value = value
+        self.queue = []
+
+    def wait(self, current):
+        if self.value > 0:
+            self.value -= 1
+            return True
+        else:
+            self.queue.append(current)
+            return False
+
+    def signal(self):
+        if self.queue:
+            next_thread = self.queue.pop(0)
+            scheduler.insert(0, next_thread)
+        else:
+            self.value += 1
+
+
+
+
+# Simple Mutex
+class Mutex:
+    def __init__(self):
+        self.locked = False
+        self.queue = []
+
+    def acquire(self, current):
+        if not self.locked:
+            self.locked = True
+            return True
+        else:
+            self.queue.append(current)
+            return False
+
+    def release(self):
+        if self.queue:
+            next_thread = self.queue.pop(0)
+            scheduler.insert(0, next_thread)
+        else:
+            self.locked = False
+
+
+# Scheduler
+scheduler = []
